8860e6174243c253ada511106fae7ae07c5a879f
Nathan Lintz <nlintz@gmail.com>
2012-05-02 20:29:56 -0400

    minimum deliverable

--- ebay2g4si.user.js
+++ ebay2g4si.user.js
@@ -5,17 +5,8 @@
 // @include https://ec2.g4si.com/shipping/*
 // ==/UserScript==
 var alert_function = function() {
-	alert('swag')
-	var shipping_info_string = document.getElementById('ctl00_cphContent_txtPurchaseOrder').value;
-	if (shipping_info_string.length < 3) {
-		return;
-	}
-	if (shipping_info_string.slice(0,2) == "xx") {
-		return;
-	}
-	
+	var shipping_info_string = document.getElementById('ctl00_cphContent_dropShipEmail2').value;
 	var states = { "AL":1, "AK":2, "AZ":3, "AR":4, "CA":5, "CO":6, "CT":7, "DE":8, "DC":9, "FL":10, "GA":11, "HI":12, "ID":13, "IL":14, "IN":15, "IA":16, "KS":17, "KY":18, "LA":19, "ME":20, "MD":21, "MA":22, "MI":23, "MN":24, "MS":25, "MO":26, "MT":27, "NE":28, "NV":29, "NH":30, "NJ":31, "NM":32, "NY":33, "NC":34, "ND":35, "OH":36, "OK":37, "OR":38, "PA":39, "RI":40, "SC":41, "SD":42, "TN":43, "TX":44, "UT":45, "VT":46, "VA":47, "WA":48, "WV":49, "WI":50, "WY":51 };
-
 	var shipping_info_array = shipping_info_string.split("  "); //changed split delimiter from line break to double space
 	var length = shipping_info_array.length;
 	if (length >= 1) {
@@ -31,7 +22,7 @@ var alert_function = function() {
 	if (length >= 3) {
 		document.getElementById('ctl00_cphContent_txtCity').value = shipping_info_array[2].split(",")[0];
 		var city_state_zip_array = shipping_info_array[2].split(" ");
-		document.getElementById('ctl00_cphContent_txtZip').value = city_state_zip_array[city_state_zip_array.length - 1];//TODO make the zip shorter
+		document.getElementById('ctl00_cphContent_txtZip').value = city_state_zip_array[city_state_zip_array.length - 1].split('-')[0];//TODO make the zip shorter (unit test this fix)
 		var state = city_state_zip_array[city_state_zip_array.length - 2];
 		document.getElementById('ctl00_cphContent_ddState').options[states[state]].selected = true;
 	}
@@ -46,10 +37,9 @@ var alert_function = function() {
 	if (length >= 6) {
 		document.getElementById("ctl00_cphContent_txtEmail").value = shipping_info_array[5];
 	}
+	document.getElementById("ctl00_cphContent_dropShipment").checked = false;
 };
 
 
-
-var text_area = document.getElementByID("ctl00_cphContent_txtPurchaseOrder");
-alert('swag')
+var text_area = document.getElementById("ctl00_cphContent_dropShipEmail2");
 text_area.addEventListener("click", alert_function, false);
